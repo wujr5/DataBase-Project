e66ae32a1124cf579baf2a890fb4c40397ec0ee2
wujr5 <973430584@qq.com>
2015-06-19 13:50:37 +0800

    添加JsonBinaryConversion类借口

---
+++ project/src/json_binary_conversion.cpp
@@ -0,0 +1,9 @@
+#include "json_binary_conversion.h"
+
+void JsonBinaryConversion::j2b() {
+  cout << "json to binary" << endl;
+}
+
+void JsonBinaryConversion::b2j() {
+  cout << "binary to json" << endl;
+}
---
+++ project/src/json_binary_conversion.h
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+class JsonBinaryConversion {
+public:
+  // return type and arguments should be updated
+  void j2b();
+  void b2j();
+  // add your code below...
+};
